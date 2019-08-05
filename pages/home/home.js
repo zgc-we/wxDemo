ef345a6bc06a91c3aeb5cd3283452e3354a07557
ludan <1107743896@qq.com>
2019-08-05 22:35:39 +0800

    list 循环添加数据

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,14 +11,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let _list = [
+      {
+        titleImg: '/images/avatar/1.png',
+        titleContent: 'Nov 25 2016',
+        header:'正是虾肥蟹壮时',
+        headerImg: '/images/post/crab.png',
+        content: '整阿道夫阿斯蒂芬的说法法术打击发的说法阿斯蒂芬的说法二萨法发斯蒂芬整阿道夫阿斯蒂芬的说法法术打击发的说法阿斯蒂芬的说法二萨法发斯蒂',
+        star: '93',
+        seePeople:'65'
+      },
+      {
+        titleImg: '/images/avatar/2.png',
+        titleContent: 'Nov 25 2016',
+        header: '正是虾肥蟹壮时',
+        headerImg: '/images/post/cat.png',
+        content: '整阿道夫阿斯蒂芬的说法法术打击发的说法阿斯蒂芬的说法二萨法发斯蒂芬整阿道夫阿斯蒂芬的说法法术打击发的说法阿斯蒂芬的说法二萨法发斯蒂',
+        star: '93',
+        seePeople: '65'
+      }
+    ];
+    this.setData({list:_list})
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
